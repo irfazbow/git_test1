0a092e4d6a3766e715882b04a0bd3b4ec423c0a6
irfazbow <irfanefp@gmail.com>
2024-03-20 13:54:07 +0700

    Taking out the sout of strings from inside the for loop to outside for loop. Removed letter 'g' from the '... 2nd commitg' from line 15 (now)

--- src/Main.java
+++ src/Main.java
@@ -10,8 +10,11 @@ public class Main {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
-            System.out.println("test change prior to commit");
-            System.out.println("test change after 2nd commitg");
         }
+        System.out.println("test change prior to commit");
+        System.out.println("test change after 2nd commitg");
+        System.out.println("i = " + i);
+        System.out.println("test change prior to commit");
+        System.out.println("test change after 2nd commitg");
     }
 }
